fcbd2366547346771125bc110511c96e174281eb
Kozzuro <oli_jab_pk@wp.pl>
2022-07-11 20:57:40 +0200

    added init of mongoose mongodb

--- src/database/dog.js
+++ src/database/dog.js
@@ -1,15 +1,30 @@
 const DB = require("./db.json");
+const Dog = require("./dogModel");
+const mongoose = require("mongoose");
 const { saveToDatabase } = require("./utils");
+
+const username = "Kozzuro";
+const password = "AUeYjKGSTZ0fwYLm";
+const cluster = "cluster.6ebvk";
+const dbname = "shelter";
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to MongoDB!!!");
+  }
+);
 
 const getAllDogs = () => {
   return DB.dogs;
 };
 
 const createNewDog = (newDog) => {
-//   const isAlreadyAdded = DB.dogs.findIndex((dog) => dog.name === dog.name) > -1;
-//   if (isAlreadyAdded) {
-//     return;
-//   }
   console.log(newDog);
   DB.dogs.push(newDog);
   saveToDatabase(DB);
