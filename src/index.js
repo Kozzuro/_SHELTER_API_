cfdd77217205a76fd0bd11c2b55b205d3fabe162
Kozzuro <oli_jab_pk@wp.pl>
2022-07-15 20:04:55 +0200

    added oauth for endpoints

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-const express = require("express"); 
+const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const jwksRsa = require("jwks-rsa");
+const { expressjwt: jwt } = require('express-jwt');
 const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const v1DogRouter = require("./v1/routes/dogRoutes");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-7fcf3-i9.us.auth0.com/.well-known/jwks.json`,
+  }),
+
+  audience: "https://dev-7fcf3-i9.us.auth0.com/api/v2/",
+  issuer: `https://dev-7fcf3-i9.us.auth0.com/`,
+  algorithms: ["RS256"],
+});
+
+app.use(checkJwt);
 app.use(bodyParser.json());
 app.use("/api/v1/dogs", v1DogRouter);
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
-    V1SwaggerDocs(app, PORT);
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
